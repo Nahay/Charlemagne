8426c31ea0e95248d522c9565f8b00261ae34ab2
Lily <hayashi.quan@gmail.com>
2022-02-25 10:51:28 +0100

    fix commands list name

--- server/routes/commands.js
+++ server/routes/commands.js
@@ -16,7 +16,12 @@ router.get('/', async (req, res) => {
 // Get command by date
 router.get('/:dateC', async (req, res) => {
     try {
-        const command = await Command.find({ dateC: req.params.dateC }).populate('user').populate('list');
+        const command = await Command.find({ dateC: req.params.dateC }).populate('user').populate({
+            path : 'list',
+            populate : {
+              path : 'dishID'
+            }
+        });
         res.json(command);
     } catch(err) {
         res.json({error: err.message});
